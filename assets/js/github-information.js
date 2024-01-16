eba1e39370a71fc9b72752b22c30e5af681e4df1
Richard Marles <marleswebinfo@gmail.com>
2024-01-16 10:20:47 +0100

    Fix bugs for the gh-user-data and gh-repo-data

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -39,6 +39,8 @@ function repoInformationHTML(repos){
 
 
 function fetchGitHubInformation(event){
+    $("#gh-user-data").html(" ");
+    $("#gh-repo-data").html("  ");
 
     var username= $("#gh-username").val();
     if(!username){
@@ -75,3 +77,5 @@ function fetchGitHubInformation(event){
         }
     )
 }
+
+$(document).ready(fetchGitHubInformation);
